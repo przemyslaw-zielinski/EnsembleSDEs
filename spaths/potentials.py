Corrected potential gradient evaluation.

spaths/potentials.py
@@ -17,14 +17,16 @@ class PairwisePotential():
 
     def __call__(self, t, x):
         npart = len(x) // self.dim
-        pidx = [tuple(range(i, i+self.dim)) for i in range(npart)]
+        # pidx = [tuple(range(i, i+self.dim)) for i in range(npart)]
 
         val = 0.0
-        for idx, m in enumerate(pidx[:-1]):
-            for n in pidx[idx+1:]:
+        for i1 in range(npart-1):
+            for i2 in range(i1+1, npart):
                 # breakpoint()
-                dist = boxDist(x[m], x[n], box_length=self.box_length)
-                val += self.W(dist)
+                p1 = self.get_particle(i1, x)
+                p2 = self.get_particle(i2, x)
+                dist = box_dist(p1, p2, box_length=self.box_length)
+                val += self.W[i1][i2](dist)
 
         return val
 
@@ -41,8 +43,9 @@ class PairwisePotential():
                 p1 = self.get_particle(i1, x)
                 p2 = self.get_particle(i2, x)
 
-                dist = boxDist(p1, p2, box_length=self.box_length)
-                force = self.W[i1][i2].der(dist) * (p1 - p2) / dist
+                dist = box_dist(p1, p2, box_length=self.box_length)
+                diff = box_diff(p1, p2, box_length=self.box_length)
+                force = self.W[i1][i2].der(dist) * diff / dist
                 # breakpoint()
 
                 grad[self.dim*i1:self.dim*(i1+1)] += force
@@ -69,31 +72,31 @@ class DSPotential():
         inner_der = -2 * arg
         return 2*self.bh * (1 - arg**2) * inner_der
 
-# Weeks–Chandler–Andersen potential
 class WCAPotential():
     '''
-    Weeks-Chandler-Andersen (WCA) potential is the Lenar-Jones potential
-    truncated at the minimum potential energy at a distance
-        r = 2**(1/6) * interaction_radius
-    on the length scale and shifted upward by the amount strength
-    on the energy scale such that both the energy and force are zero at
+    Weeks-Chandler-Andersen (WCA) potential is the Lennard-Jones potential
+        eps * ((sigma/r)^12 - 2*(sigma/r)^6)
+    truncated at a distance sigma, that corresponds to the minimum potential
+    energy of LJ potential.
+    It is also shifted upward by the amount eps on the energy scale,
+    such that both the energy and force are zero at
     or beyond the cutoff distance.
     '''
-    def __init__(self, strength, interaction_radius):
+    def __init__(self, strength, interaction_distance):
         self.s = strength
-        self.ir = interaction_radius
+        self.id = interaction_distance
 
     def __call__(self, dist):
-        # cutoff_dist = np.maximum(dist, 2**(1/6)*self.ir)
-        if dist <= 2**(1/6)*self.ir:
-            return 4*self.s*((self.ir/dist)**12 - (self.ir/dist)**6) + self.s
+        if dist <= self.id:
+            r = self.id / dist
+            return self.s*(r**12 - 2*r**6 + 1)
         else:
             return 0.0
 
     def der(self, dist):
-        # return -24*self(distance) / distance
-        if dist <= 2**(1/6)*self.ir:
-            return -24*self.s*(2*(self.ir/dist)**11 - (self.ir/dist)**5)*self.ir
+        if dist <= self.id:
+            r = self.id / dist
+            return -12*self.s * (r**13 - r**7) / self.id
         else:
             return 0.0
 
@@ -117,7 +120,7 @@ def initialize_particles(nparts, V, inv_temp, dt, nsteps, rng):
     relax_sol = spaths.EMSolver(relax_sde, unif_ens0, (0.0, t_relax), dt, rng)
 
     # dimer_distance = [
-    #     boxDist(x[0,0:2], x[0,2:4], box_length=V.box_length)
+    #     box_dist(x[0,0:2], x[0,2:4], box_length=V.box_length)
     #     for x in relax_sol.x
     # ]
     # plt.plot(relax_sol.t, dimer_distance, alpha=.6)
@@ -125,7 +128,14 @@ def initialize_particles(nparts, V, inv_temp, dt, nsteps, rng):
 
     return relax_sol.x[nsteps]
 
-def boxDist(p1, p2, box_length=1):
+def box_fold(x, box_length=1):
+    return np.remainder(x, box_length)
+
+def box_diff(p1, p2, box_length=1):
     dp = p1 - p2
     dp -= np.rint(dp / box_length) * box_length
+    return dp
+
+def box_dist(p1, p2, box_length=1):
+    dp = box_diff(p1, p2, box_length)
     return np.linalg.norm(dp)

tests/dimer_in_2Dsolvent.py
@@ -17,29 +17,61 @@ import spaths
 file_name = "dimer_in_2Dsolvent"
 
 # seed setting
-seed = 4321
+seed = 3579
 rng = np.random.RandomState(seed)
 rng.randint(10**5, size=10**3)  # warm up of RNG
-
-# def boxFold(x, box_length=1):
-#     return np.remainder(x, box_length)
 
 
 # model parameters
 nparts = 10
 box_length = 10
-dimer = {0, 1}  # indices of particle forming the dimer
+dimer = {0, 1}  # indices of particles forming the dimer
 inv_temp = 0.5
 
 # potentials
-strength = 0.1
-interaction_radius = 1
-wca = potentials.WCAPotential(strength, interaction_radius)
+strength = 1.0
+interaction_distance = 1.0
+wca = potentials.WCAPotential(strength, interaction_distance)
 
 barrier_height = 10.0
-compact_state = 2**(1/6)*interaction_radius  # the same as wca cutoff distance
+compact_state = interaction_distance  # the same as wca cutoff distance
 loose_state = compact_state + 1.4
 ds = potentials.DSPotential(barrier_height, compact_state, loose_state)
+
+Wmat = [# only for the dimer use the ds potential
+    [ds if {m, n} == dimer else wca for m in range(nparts)]
+    for n in range(nparts)
+]
+V = potentials.PairwisePotential(Wmat, dim=2, box_length=box_length)
+
+# simulation parameters
+dt = 1e-4
+nsam = 1
+tspan = (0.0, 10.0)
+
+# gentle initialization of particles
+nsteps = 200
+ens0 = potentials.initialize_particles(nparts, V, inv_temp, dt, 200, rng)
+
+sde = spaths.OverdampedLangevin(V, inv_temp)
+sol = spaths.EMSolver(sde, ens0, tspan, dt, rng)
+
+# plot evolution of dimer length
+fig, ax = plt.subplots(figsize=(8,6))
+ls = 16
+lw = 2
+
+dimer_distance = [
+    potentials.box_dist(x[0,0:2], x[0,2:4], box_length=box_length)
+    for x in sol.x
+]
+step = 100
+ax.plot(sol.t[::step], dimer_distance[::step], alpha=.6)
+
+
+fig.tight_layout()
+fig.savefig(f"figs/{file_name}.pdf")
+plt.close(fig)
 
 # plot potentials
 d_plot = np.linspace(0.1, 1.2*loose_state, 200)
@@ -56,36 +88,3 @@ fig.tight_layout()
 fig.savefig(f"figs/potentials.pdf")
 plt.close(fig)
 ################
-
-Wmat = [# only for dimer use the ds potential
-    [ds if {m, n} == dimer else wca for m in range(nparts)]
-    for n in range(nparts)
-]
-V = potentials.PairwisePotential(Wmat, dim=2, box_length=box_length)
-
-# simulation parameters
-dt = 1e-4 / 2.0
-nsam = 1
-tspan = (0.0, 2.0)
-
-sde = spaths.OverdampedLangevin(V, inv_temp)
-ens0 = potentials.initialize_particles(nparts, V, 2*inv_temp, dt, 200, rng)
-sol = spaths.EMSolver(sde, ens0, tspan, dt, rng)
-# print(sol.x.shape)
-
-fig, ax = plt.subplots(figsize=(8,6))
-ls = 16
-lw = 2
-
-dimer_distance = [
-    potentials.boxDist(x[0,0:2], x[0,2:4], box_length=box_length)
-    for x in sol.x
-]
-
-step = 100
-ax.plot(sol.t[::step], dimer_distance[::step], alpha=.6)
-
-
-fig.tight_layout()
-fig.savefig(f"figs/{file_name}.pdf")
-plt.close()