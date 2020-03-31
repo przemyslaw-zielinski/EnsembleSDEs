Corrected computation of propensities for Chemical Langevin and updated dimerization test.

spaths/systems/chemical_langevin.py
@@ -14,24 +14,18 @@ from scipy.special import comb
 
 
 class ChemicalLangevin(ItoSDE):
+    '''
+    Implements
+        dY = {S @ a(Y)}dt + {S * sqrt(a(Y))}dW
+    '''
 
     def __init__(self, nb_species, ls_reactions):
 
         self.nb_species = nb_species
         self.nb_reactions = len(ls_reactions)
 
-        ls_rates = []
-        ls_idxs  = []
-        ls_coeff = []
-        for react in ls_reactions:
-            ls_rates.append(react.rate)
-            ls_idxs.append([subs.species_id for subs in react.substrates])
-            ls_coeff.append([subs.coeff for subs in react.substrates])
-        self.ar_rates = np.array(ls_rates)[:, np.newaxis]
-        self.ar_idxs  = np.array(list(zip_longest(*ls_idxs, fillvalue=nb_species)))
-        self.ar_coeff = np.array(list(zip_longest(*ls_coeff, fillvalue=0)))
-
-        self.sm_mat = self.generate_stoichiometric_matrix(ls_reactions)
+        self.generate_propensity_parameters(ls_reactions)
+        self.generate_stoichiometric_matrix(ls_reactions)
 
         super().__init__(self.cl_drift, self.cl_dispersion,
                          noise_mixing_dim=self.nb_reactions)
@@ -47,22 +41,52 @@ class ChemicalLangevin(ItoSDE):
         prop = np.ones((self.nb_reactions, x.shape[1]))
         # breakpoint()
         prop = self.ar_rates * prop
-        for idxs, coeff in zip(self.ar_idxs, self.ar_coeff):
-            active = idxs < self.nb_species
-            idxs = idxs[active]
-            coeff = coeff[active][:, np.newaxis]
-            prop *= comb(x[idxs], coeff)
+        #  we iterate over the number of substrates
+        for idcs, coeffs in zip(self.ar_subs_idcs, self.ar_subs_coeffs):
+            # breakpoint()
+            active = idcs < self.nb_species
+            idcs = idcs[active]
+            coeffs = coeffs[active][:, np.newaxis]
+            prop[active] *= comb(x[idcs], coeffs)
 
         return prop
 
     def generate_stoichiometric_matrix(self, ls_reactions):
 
-        sm_matrix = []
+        sm_mat = []
         for react in ls_reactions:
-            v = np.zeros(self.nb_species)  # stoichiometric vector
+            sm_vec = np.zeros(self.nb_species)  # stoichiometric vector
             for species_idx, coeff in react.substrates:
-                v[species_idx] = -coeff  # substrts are used up
+                sm_vec[species_idx] = -coeff  # substrates are used up
             for species_idx, coeff in react.products:
-                v[species_idx] = +coeff  # products are created
-            sm_matrix.append(v)
-        return np.array(sm_matrix).T
+                sm_vec[species_idx] = +coeff  # products are created
+            sm_mat.append(sm_vec)
+
+        self.sm_mat = np.array(sm_mat).T  # sm_vec's are columns of sm_mat
+
+    def generate_propensity_parameters(self, ls_reactions):
+        '''
+        Generates three arrays:
+            - ar_rates:       with shape (nb_reactions, 1)
+                              storing the rates of all reactions
+            - ar_subs_idcs:   with shape (max_nb_substrates, nb_reactions)
+                              storing the indices of substrates for all reactions
+            - ar_subs_coeffs: with shape (max_nb_substrates, nb_reactions)
+                              storing the coefficients
+        '''
+
+        ls_rates, ar_subs_idcs, ls_subs_coeffs = [], [], []
+        for react in ls_reactions:
+            ls_rates.append(react.rate)
+            ar_subs_idcs.append([subs.species_id for subs in react.substrates])
+            ls_subs_coeffs.append([subs.coeff for subs in react.substrates])
+
+        self.ar_rates = np.array(ls_rates)[:, np.newaxis]
+
+        # both 'ar_subs_idcs' and ' ar_subs_coeffs' have shape
+        #   (max(nb_substrates), nb_reactions)
+        # row s stores sth substrate indices and coefficients for all reactions
+        self.ar_subs_idcs  = np.array(list(zip_longest(*ar_subs_idcs,
+                                                  fillvalue=self.nb_species)))
+        self.ar_subs_coeffs = np.array(list(zip_longest(*ls_subs_coeffs,
+                                                   fillvalue=0)))

tests/dimerization.py
@@ -14,40 +14,51 @@ import math
 cwd = os.getcwd()
 sys.path.append(cwd + '/..')
 import spaths
-
+from spaths.reactions import intermediate, Reaction
 
 # seed setting
-seed = 357
+seed = 3579
 rng = np.random.default_rng(seed)
 rng.integers(10**3, size=10**3)  # warm up of RNG
 
 # simulation parameters
 dt = 1e-4
-nsam = 100
+nsam = 10
 tspan = (0.0, 10.0)
 
 # initial conditions
 x0, y0 = [100.0]*nsam, [100.0]*nsam
 
-k1 = 1.0
-k2 = 100.0
-k3 = 50.0
+Y  = intermediate(1)
+X  = intermediate(0)
+X2 = intermediate(0, 2)
 
-def drift(t, u, du):
-    du[0] = 2*k2*u[1] - 2*k1*u[0]*(u[0]-1) + k3
-    du[1] = k1*u[0]*(u[0]-1) - k2*u[1]
+c1 = 1.0
+c2 = 100.0
+c3 = 50.0
 
-def dispersion(t, u, du):
-    du[0,0] = -2*np.sqrt(k1*u[0]*(u[0]-1))
-    du[0,1] = 2*np.sqrt(k2*u[1])
-    du[0,2] = np.sqrt(k3)
-    du[1,0] = np.sqrt(k1*u[0]*(u[0]-1))
-    du[1,1] = -np.sqrt(k2*u[1])
-    du[1,2] = 0
+dimerization = Reaction(c1, [X2], [Y])
+dissociation = Reaction(c2, [Y], [X2])
+production   = Reaction(c3, [], [X])
 
-sde = spaths.SDE(drift, dispersion, 3)
+print(production)
+
+# def drift(t, u, du):
+#     du[0] = 2*k2*u[1] - 2*k1*u[0]*(u[0]-1) + k3
+#     du[1] = k1*u[0]*(u[0]-1) - k2*u[1]
+#
+# def dispersion(t, u, du):
+#     du[0,0] = -2*np.sqrt(k1*u[0]*(u[0]-1))
+#     du[0,1] = 2*np.sqrt(k2*u[1])
+#     du[0,2] = np.sqrt(k3)
+#     du[1,0] = np.sqrt(k1*u[0]*(u[0]-1))
+#     du[1,1] = -np.sqrt(k2*u[1])
+#     du[1,2] = 0
+#
+# sde = spaths.SDE(drift, dispersion, 3)
+cle = spaths.ChemicalLangevin(2, [dimerization, dissociation, production])
 ens0 = spaths.make_ens(x0, y0)
-sol = spaths.EMSolver(sde, ens0, tspan, dt, rng)
+sol = spaths.EMSolver(cle, ens0, tspan, dt, rng)
 
 fig, ax = plt.subplots(figsize=(8,6))
 ls = 16
@@ -55,10 +66,12 @@ lw = 2
 
 tplot = sol.t[::50]
 splot = sol(tplot)
-ax.plot(tplot, splot[:,11,1], color="C1", alpha=.5)
-ax.plot(tplot, splot[:,11,0], color="C0", alpha=.5)
+ax.plot(tplot, splot[:,2,1], color="C1", alpha=.5)
+ax.plot(tplot, splot[:,2,0], color="C0", alpha=.5)
 
-# slow_avg = np.average(solution[idx,:,0], axis=1)
+slow_avg = np.average(splot[:,:,0] + 2*splot[:,:,1], axis=1)
+ax.axhline(y=c3, color='k', linestyle='--')
+ax.plot(tplot[1:], (slow_avg[1:] - 300) / tplot[1:])
 # slow_std = np.std(solution[idx,:,0], axis=1)
 # ax.plot(tgrid[idx], slow_avg, color="C0", linewidth=lw)
 # ax.fill_between(tgrid[idx], slow_avg - slow_std, slow_avg + slow_std,

tests/michaelis–menten.py
@@ -38,7 +38,8 @@ vol = 1e-15  # volume of system
 y0 = np.zeros(4)
 y0[0] = np.round(5e-7*nA*vol)  # molecules of substrate S
 y0[1] = np.round(2e-7*nA*vol)  # molecules of enzyme E
-ens0 = y0.reshape(1, 4)
+ens0 = np.tile(y0, (5, 1))
+# print(ens0)
 
 c1, c2, c3 = 1e6/(nA*vol), 1e-4, 0.1
 
@@ -54,12 +55,14 @@ t_span = (0.0, 50.0)
 dt = t_span[1] / 250
 
 cle = spaths.ChemicalLangevin(4, [binding, dissociation, product])
+# print(cle.ar_idxs)
+# print(cle.ar_coeff)
 sol = spaths.EMSolver(cle, ens0, t_span, dt, rng)
 
 
 fig, ax = plt.subplots(figsize=(8,6))
 
-ax.plot(sol.t, sol.p[0])
+ax.plot(sol.t, sol.p[1])
 
 fig.tight_layout()
 fig.savefig(f"figs/{file_name}.pdf")