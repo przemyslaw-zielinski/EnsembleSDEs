663b982472eaaf7485734e0b07205fe4913db113
Przemyslaw Zielinski <pzielinski@mathpc21.local>
2020-04-03 14:23:03 +0200

    Added class implementing Ornstein-Uhlenbeck process.

--- spaths/__init__.py
+++ spaths/__init__.py
@@ -14,3 +14,4 @@ from spaths.solvers import EMSolver, make_ens
 from spaths.systems.ito import ItoSDE
 from spaths.systems.overdamped_langevin import OverdampedLangevin
 from spaths.systems.chemical_langevin import ChemicalLangevin
+from spaths.systems.ornstein_uhlenbeck import OrnsteinUhlenbeck
