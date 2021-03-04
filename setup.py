b82ddca5f5202d5e6a0d91485d802622c2edca76
pz <zielinski.przemek@gmai.com>
2021-03-04 11:25:40 +0100

    switched to environment.yaml, updated setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
+from spaths.version import __version__
 
-setup(name='spaths',
-      version='0.1',
-      description='Ensemble simulation of stochastic processes',
-      url='',
-      author='Przemyslaw Zielinski',
-      author_email='zielinski.przemek@gmail.com',
-      license='CC',
-      packages=['spaths'],
-      zip_safe=False)
+setup(
+    name='spaths',
+    version=__version__,
+    description='Ensemble simulation of stochastic processes',
+    url='',
+    author='Przemyslaw Zielinski',
+    author_email='zielinski.przemek@gmail.com',
+    license='CC',
+    packages=['spaths'],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "jax"
+    ],
+    zip_safe=False
+)
